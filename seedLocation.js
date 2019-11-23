83c07959a8979158f7375c3625e2bb5019a23a97
William Hudson <williamdhudson@gmail.com>
2019-11-23 14:59:58 -0500

    Created package.json

--- seedLocation.js
+++ seedLocation.js
@@ -23,37 +23,37 @@ var locations = [];
 ​
 // Helper function to keep the coordinates within the US
 var randomCoordinates = (min, max) => {
-  return min + Math.random()*(max + 1 -min);
+  return min + Math.random()*(max - min);
 }
 
 for (let i = 0; i < 100; i++) {
     let eventId = i;
-    let Location_name = faker.company.companyName();
-    let Address_1 = faker.address.streetAddress();
-    let Address_2 = faker.address.secondaryAddress();
-    let City = faker.address.city();
-    let State = faker.address.stateAbbr();
-    let Zip = faker.address.zipCode();
-    let Country = faker.address.country();
+    let location_name = faker.company.companyName();
+    let address_1 = faker.address.streetAddress();
+    let address_2 = faker.address.secondaryAddress();
+    let city = faker.address.city();
+    let state = faker.address.stateAbbr();
+    let zip = faker.address.zipCode();
+    let country = faker.address.country();
     // These coordinates will keep the latitude and Longitude inside the US
-    let Lat = randomCoordinates(30, 48);
-    let Long = randomCoordinates(-80, -117);
+    let lat = randomCoordinates(30, 48);
+    let long = randomCoordinates(-80, -117);
     let how_to_find_us = faker.lorem.sentence();
 ​
     let eventLocation = {
         eventId,
-        Location_name,
-        Address: {
-            Address_1,
-            Address_2,
-            City,
-            State,
-            Zip,
-            Country
+        location_name,
+        address: {
+            address_1,
+            address_2,
+            city,
+            state,
+            zip,
+            country
         },
-        Geo: {
-            Lat,
-            Long
+        geo: {
+            lat,
+            long
         },
         how_to_find_us
     }
