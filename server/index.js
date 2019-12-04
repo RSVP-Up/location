429dc18f25dd08d3c8ed7248e0cceaee1d91ef04
William Hudson <williamdhudson@gmail.com>
2019-12-03 21:53:29 -0500

    Server running

--- server/index.js
+++ server/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 let app = express();
-const db = require('../database/inex.js');
+// const db = require('../database/index.js');
+let PORT = 3010;
 
-app.use(express.static(__dirname + '/../client/dist'));
 app.use(bodyParser.json());
+app.use(express.static(__dirname + '/../client/dist'));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
